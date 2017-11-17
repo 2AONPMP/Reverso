c9e612d5a101282b68f30a8ab199ec539efc392a
eleves <eleves@alessandra-2.local>
2017-11-17 12:03:33 -0500

    Reverso_constraint

--- Reverso/DeuxLangues.swift
+++ Reverso/DeuxLangues.swift
@@ -8,10 +8,11 @@ import UIKit
 
 class DeuxLangues: UIViewController {
     
-  
+    // Déclaration des boutons en "Français" et "Anglais" pour choisir quels mots seront préesentées dnas le tableau.
     @IBOutlet weak var fr: UITextField!
     @IBOutlet weak var en: UITextField!
     
+    // Déclaration des tables qui sauveront les mots en "Français" et "Anglais"
     var arrFrench: [String]!
     var arrEnglish: [String]!
     
@@ -22,13 +23,17 @@ class DeuxLangues: UIViewController {
     }
     
     //--------------------
+    
     // Fonction pour ajouter les nouvelles mots en addition dans le dictionnaire
-
     @IBAction func add(_ sender: UIButton) {
+        // Ajoute des mots en Français
         arrFrench.append(fr.text!)
+        // Ajoute des mots en Anglais
         arrEnglish.append(en.text!)
         
+        // Insertion des mots en Français
         UserDefaults.standard.set(arrFrench, forKey: "FRANÇAIS")
+         // Insertion des mots en Anglais
         UserDefaults.standard.set(arrEnglish, forKey: "ANGLAIS")
     }
     
